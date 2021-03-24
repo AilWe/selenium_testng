b70dd9ae972c694574e41d686034c95b6716ce55
AileenWei <aileen@minorityprogrammers.org>
2021-03-23 20:42:18 -0400

    print link

--- src/test/java/koinstreet_learn_app/Navigation.java
+++ src/test/java/koinstreet_learn_app/Navigation.java
@@ -52,6 +52,13 @@ public class Navigation {
     @Test(priority = 1, description = "Navigation at Top Home Symbol test")
     public void NavigationAtTopHomeSymbol(){
         driver.findElement(By.cssSelector(".active > img")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to home page successfully");
 
     }
@@ -59,6 +66,13 @@ public class Navigation {
     @Test(priority = 2, description = "Navigation at Top Course test")
     public void NavigationAtTopCourse(){
         driver.findElement(By.cssSelector(".navigation__wrapper .active")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to home page successfully");
 
     }
@@ -67,18 +81,39 @@ public class Navigation {
     public void NavigationAtTopAboutUs(){
 
         driver.findElement(By.cssSelector(".navigation__wrapper > li:nth-child(3) > a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to about us successfully");
     }
 
     @Test(priority = 4, description = "Navigation at Top Contact Us test")
     public void NavigationAtTopContactUs(){
         driver.findElement(By.cssSelector("li:nth-child(4) > a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to contact us successfully");
     }
 
     @Test(priority = 5, description = "Navigation at Bottom Home Symbol test")
     public void NavigationAtBottomHomeSymbol(){
         driver.findElement(By.cssSelector(".col-3 > a > img")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to home page successfully");
     }
 
@@ -86,12 +121,26 @@ public class Navigation {
     public void NavigationAtBottomHome(){
 
         driver.findElement(By.cssSelector(".col-3:nth-child(2) > a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to home page successfully");
     }
 
     @Test(priority = 7, description = "Navigation at Bottom About Us test")
     public void NavigationAtBottomAboutUs(){
         driver.findElement(By.cssSelector(".col-3:nth-child(2) > a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to about us successfully");
     }
 
@@ -99,6 +148,13 @@ public class Navigation {
     public void NavigationAtLearnMore(){
 
         driver.findElement(By.linkText("LEARN MORE")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to about us successfully");
     }
 
@@ -106,12 +162,26 @@ public class Navigation {
     public void NavigationAtViewLesson1(){
 
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[6]/div/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Lesson 1 successfully");
     }
     @Test(priority = 10, description = "Navigation At Start Course 1")
     public void NavigationAtStartCourse1(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[6]/div/div/a")).click();
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Course 1 successfully");
     }
 
@@ -121,24 +191,52 @@ public class Navigation {
     public void NavigationAtViewLesson2(){
 
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[7]/div/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Lesson 2 successfully");
     }
     @Test(priority = 12, description = "Navigation At Start Course 2")
     public void NavigationAtStartCourse2(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[7]/div/div/a")).click();
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Course 2 successfully");
     }
 
     @Test(priority = 13, description = "Navigation At View Lesson 3")
     public void NavigationAtViewLesson3(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[8]/div/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Lesson 3 successfully");
     }
     @Test(priority = 14, description = "Navigation At Start Course 3")
     public void NavigationAtStartCourse3(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[8]/div/div/a")).click();
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Course 3 successfully");
     }
 
@@ -146,6 +244,13 @@ public class Navigation {
     public void NavigationAtViewLesson4(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[2]/li[2]/button")).click();
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[9]/div/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Lesson 4 successfully");
     }
 
@@ -154,6 +259,13 @@ public class Navigation {
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[2]/li[2]/button")).click();
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[9]/div/div/a")).click();
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Course 4 successfully");
     }
 
@@ -161,6 +273,13 @@ public class Navigation {
     public void NavigationAtViewLesson5(){
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[2]/li[2]/button")).click();
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[10]/div/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Lesson 5 successfully");
     }
 
@@ -170,6 +289,13 @@ public class Navigation {
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[2]/li[2]/button")).click();
         driver.findElement(By.xpath("/html/body/div/section[3]/div/div/div/ul[1]/li[10]/div/div/a")).click();
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/div/a")).click();
+
+        String currentUrl =  driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
         System.out.println("Navigation to Course 5 successfully");
     }
 
